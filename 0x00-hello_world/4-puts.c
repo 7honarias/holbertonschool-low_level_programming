9475f25ad598304f6d276bd7a6187e9201d62a58
7honarias <7honarias@gmail.com>
2020-09-16 21:48:26 +0000

    [ADD] - Exercise 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ *  main is the principal fun
+ *  puts show de text 
+ *  return 0 is true
+ */
+
+
+int main(void)
+{
+  puts("Progrmming is like building a multilingual puzzle");
+  
+  return 0;
+
+}
