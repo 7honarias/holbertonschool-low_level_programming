274ce532421a12ba816dcd5f2118ed7418cdc27e
7honarias <7honarias@gmail.com>
2020-09-16 23:01:03 +0000

    [ADD] - exercise 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
+ * main - Entry point
  *
- * printf show the text
- *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
