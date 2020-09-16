19848a75f49ef9ff8df938eb39a525c713f343d9
7honarias <7honarias@gmail.com>
2020-09-16 21:59:35 +0000

    Correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
-/*
- *  main is the principal fun
- *  puts show de text 
- *  return 0 is true
- */
-
-
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
-  
   return (0);
-
 }
