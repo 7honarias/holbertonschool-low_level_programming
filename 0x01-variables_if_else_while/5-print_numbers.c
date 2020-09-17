00cbabfdbb76a2d21c1a7a8b6bfa4564d67933bc
7honarias <7honarias@gmail.com>
2020-09-17 19:14:35 +0000

    [ADD] - exercise 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+  int a;
+  for (a = 0; a < 10; a++)
+    {
+      printf("%i",a);
+    }
+  putchar('\n');
+  return (0);
+}
