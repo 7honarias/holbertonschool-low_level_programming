a01b8751c9cc53452d72eac340f7dcb1c4fae643
7honarias <7honarias@gmail.com>
2020-09-17 19:21:00 +0000

    Correction

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,11 +6,11 @@
 #include <stdio.h>
 int main(void)
 {
-  int a;
-  for (a = 0; a < 10; a++)
-    {
-      printf("%i", a);
-    }
-  putchar('\n');
-  return (0);
+int a;
+for (a = 0; a < 10; a++)
+{
+printf("%i", a);
+}
+putchar('\n');
+return (0);
 }
