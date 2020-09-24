4b33a82bc48304d798d826a1f49476619d697b8f
7honarias <7honarias@gmail.com>
2020-09-24 18:49:34 +0000

    Correction 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,13 +10,14 @@
 int main(void)
 {
 	int numb;
+
 	for (numb = 1; numb <= 100; numb++)
 	{
 		if ((numb % 3 == 0) && (numb % 5 == 0))
 		{
 			printf("FizzBuzz ");
 		}
-		else if(numb % 3 == 0)
+		else if (numb % 3 == 0)
 		{
 			printf("Fizz ");
 		}
