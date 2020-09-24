9991020a6ce2181a84770bdcfe7954b4528cb303
7honarias <7honarias@gmail.com>
2020-09-24 18:36:32 +0000

    Correction 0,2

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * _isdigit - Check if a character is a digit.
+ * _isupper - Check if a character is a digit.
  * @c: integer to evaluate
  * Return: 1 if c is a uppcase, 0 otherwise.
  */
