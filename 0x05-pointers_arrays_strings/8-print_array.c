feef5c7c2530d0e05c2f921ae59e095608df9842
7honarias <7honarias@gmail.com>
2020-09-29 12:26:27 +0000

    Correction 8,1

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,24 +1,23 @@
-# include <stdio.h>
-# include "holberton.h"
+#include "holberton.h"
+#include <stdio.h>
 
 /**
- * print_array - print array
- * @a: array
- * @n: number to print
- * Return: Nothing
+ * print_array - print n .
+ * @a: take a pointer, string.
+ * @n: take a int
+ * Return:string
  */
-
 void print_array(int *a, int n)
 {
-	int cont;
+	int size;
 
-	for (cont = 0; cont < n; cont++)
+	for (size = 0; size < n; size++)
 	{
-		printf("%d", a[cont]);
-		if (cont < (n - 1))
+		printf("%d", a[size]);
+		if (size < (n - 1))
 		{
 			printf(", ");
 		}
 	}
-	printf('\n');
+	printf("\n");
 }
