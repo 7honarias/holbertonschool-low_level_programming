180348b94ddc9ae3bdba169044e3a8e22cde27e7
7honarias <7honarias@gmail.com>
2020-10-01 21:12:54 +0000

    [ADD] - Exercise 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+# include "holberton.h"
+
+/**
+ * string_toupper - changes all lowercase letters
+ * @s:
+ * Return: char.
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		if (*s >= 97 && *s <= 122)
+		{
+
+			*s = *s - 32;
+		}
+		i++;
+		s++;
+	}
+	return(s);
+}
