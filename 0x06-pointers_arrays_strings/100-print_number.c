3eb244c2e91d46a2f42b1de6671c61df691189d1
7honarias <7honarias@gmail.com>
2020-10-02 17:25:46 +0000

    [ADD] - Exercise 100

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,29 @@
+# include "holberton.h"
+
+/**
+ * print_number - print the number
+ * @n: number to print
+ * Return: Nothing
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+	if (n > 999)
+	{
+		_putchar((n / 1000) % 10 + '0');
+	}
+	if (n > 99)
+	{
+		_putchar((n / 100) % 10 + '0');
+	}
+	if (n > 9)
+	{
+		_putchar((n / 10) % 10 + '0');
+	}
+	_putchar(n % 10 + '0');
+}
