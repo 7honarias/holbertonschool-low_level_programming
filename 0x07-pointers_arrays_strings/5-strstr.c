25a104c2914e11b1343aa373c533ca9844148c6e
7honarias <7honarias@gmail.com>
2020-10-05 21:25:12 +0000

    [ADD] - Exercise 5-strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+# include "holberton.h"
+/**
+ * strstr - definition
+ * @haystack: firt string
+ * @needle: second string
+ * Return: null if not found or pointer
+ */
+
+char *strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; haystack[i]; i++)
+	{
+		j = 0;
+		while (needle[j] != '/0' && haystack[i] == needle[j])
+		{
+			j++;
+			i++;
+		}
+
+		if (j > 0)
+		{
+			i -= j;
+			if (needle[j] == '/0')
+			{
+				return (haystack + i);
+			}
+		}
+	}
+	return ('\0');
+}
