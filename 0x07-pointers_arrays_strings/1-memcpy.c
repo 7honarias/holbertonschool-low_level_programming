ef60075f04ead0fe8cf7072928c730fca777e742
7honarias <7honarias@gmail.com>
2020-10-05 20:14:33 +0000

    [ADD] - 1-memcpy Exercise

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+# include "holberton.h"
+
+/**
+ * _memcpy - funtion thar copies memory area.
+ * @dest: string destin
+ * @src: string src
+ * @n: number
+ * Return: pointer to dest.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		*dest = src[i];
+		++dest;
+		i++;
+	}
+	return (dest);
+}
