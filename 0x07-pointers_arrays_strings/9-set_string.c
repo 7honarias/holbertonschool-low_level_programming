dc87fcbd9f648aa04396824e26b5d1bf726b2496
7honarias <7honarias@gmail.com>
2020-10-06 05:02:23 +0000

    final

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -2,7 +2,7 @@
 /**
  * set_string - sets the value of a pointer to char
  * @s: char
- * to: where pointer change
+ * @to: where pointer change
  * Return: nothing.
  */
 void set_string(char **s, char *to)
