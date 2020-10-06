603e2a76af42e955275a69ec268514a15cfdc262
7honarias <7honarias@gmail.com>
2020-10-06 01:40:25 +0000

    [ADD] - exercise 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+# include "holberton.h"
+/**
+ * print_chessboard - print a chessboard
+ * @a: array double
+ * Return: nothing
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	i = 0;
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -7,6 +7,6 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strstr(char *haystack, char *needle);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
-
+void print_chessboard(char (*a)[8]);
 
 #endif
