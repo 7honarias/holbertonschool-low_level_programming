0540f7416b1f6e363fc02ccc3e5b28740b594e46
7honarias <7honarias@gmail.com>
2020-10-11 14:12:17 +0000

    [ADD] - Exercise 1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+# include "holberton.h"
+
+/**
+ * main - print number of params
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[]__atribute__((unused)))
+{
+	printf("%d\n", (argc - 1));
+	return (0);
+}
