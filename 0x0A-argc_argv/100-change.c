a1b03891d9a0154e014817c17a0e5951c674afb5
7honarias <7honarias@gmail.com>
2020-10-11 21:55:13 +0000

    [ADD] - exercise 100

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,77 @@
+# include "holberton.h"
+# include <stdlib.h>
+
+/**
+ * calcular - calculate coins
+ * @i: int
+ * Return: int with res
+ */
+
+int calcular(int i)
+{
+	int j = i;
+	int res;
+	int k;
+
+	k = 0;
+	res = 0;
+
+	if (j >= 25)
+	{
+		res = j / 25;
+		j = j % 25;
+		k = k + res;
+	}
+
+	if (j >= 10)
+	{
+		res = j / 10;
+		j = j % 10;
+		k = k + res;
+	}
+	if (j >= 5)
+	{
+		res = j / 5;
+		j = j % 5;
+		k = k + res;
+	}
+	k = k + j;
+
+	return (k);
+}
+/**
+ * main - print the minimum number of coins
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if is success and 1 for error
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = atoi(argv[1]);
+	int rest;
+	int j = argc;
+
+	rest = 0;
+	if (j != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		if (i <= 0)
+		{
+			printf("%d\n", 0);
+			return (0);
+		}
+		else
+		{
+
+			rest = calcular(i);
+
+			printf("%d\n", rest);
+		}
+	}
+	return (0);
+}
