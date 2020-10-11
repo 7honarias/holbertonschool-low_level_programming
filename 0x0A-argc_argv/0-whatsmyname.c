2c7815df38e2a156a5072b570a4e966279af93e8
7honarias <7honarias@gmail.com>
2020-10-11 13:29:36 +0000

    Correction

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,11 +2,13 @@
 
 /**
  * main - prints name program
- *
+ * @argc: argument count
+ * @argv: argument vector
  * Return: always 0 success
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
+
 	printf("%s\n", argv[0]);
 }
