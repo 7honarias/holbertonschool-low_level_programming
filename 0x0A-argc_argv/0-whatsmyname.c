bf8f3f28a2945c95c8484e9062636e1286ceea27
7honarias <7honarias@gmail.com>
2020-10-11 13:24:37 +0000

    [ADD] - 0-whatismyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+/**
+ * main - prints name program
+ *
+ * Return: always 0 success
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+argc, argv
+
+0 - what is my name
