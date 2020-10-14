3b4bab919ec34829d701a2896acd9e4008ab6f4c
7honarias <7honarias@gmail.com>
2020-10-14 01:35:11 +0000

    [ADD] - Exercise 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+# include "holberton.h"
+
+/**
+ * _strdup - newly allocated space
+ * @str: array
+ * Return: a pointer
+ *
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0;
+	char *p;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	p = malloc(i + 1);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (s = 0; s < i; s++)
+	{
+		p[s] = str[s];
+	}
+	p[s] = '\0';
+	return (p)
+}
