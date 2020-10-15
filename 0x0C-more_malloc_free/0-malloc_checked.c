e229f72a3b224806a499207a12b2b51a7fb10a93
7honarias <7honarias@gmail.com>
2020-10-15 18:55:33 +0000

    [ADD] - Exercise 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,25 @@
+# include "holberton.h"
+#include <stdio.h>
+# include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memory using malloc
+ * @b: value to work
+ * Return: Nothing
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+	
+
+
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+
+	return(p);
+}
