1ba485d7af3bf1ddb348be483fa906a67fd14d4c
Jhon Arias <7honarias@gmail.com>
2020-10-19 20:30:35 +0000

    [ADD] - Exercise dog.h 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _MY_DOG_H
+#define _MY_DOG_H
+
+/**
+ * struct dog - struct for dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
