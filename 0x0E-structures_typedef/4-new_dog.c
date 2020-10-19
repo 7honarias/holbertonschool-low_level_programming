b702550d1f80b30dc2479fa663f11ff78dc4bf11
Jhon Arias <7honarias@gmail.com>
2020-10-19 22:42:40 +0000

    Correction 4.3

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,7 +16,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog;
 
 	if (name == NULL)
-		if(owner == NULL)
+		if (owner == NULL)
 			return (NULL);
 
 	dog = malloc(sizeof(dog_t));
