5a1704e68849a11c83445b28f75b1ad3fa13fa62
Jhon Arias <7honarias@gmail.com>
2020-10-21 21:43:13 +0000

    [ADD] - Exercise 2 index int

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,37 @@
+# include "function_pointers.h"
+# include <stdlib.h>
+
+/**
+ * int_index - searches for an integer.
+ * @array: array integer
+ * @size: size of arrays
+ * @cmp: function compare.
+ * Return: int
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	int cont = 0;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{
+			cont++;
+		}
+	}
+	if (cont > 0)
+	{
+		return (cont);
+	}
+
+	return (0);
+
+
+}
