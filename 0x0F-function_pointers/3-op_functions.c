f305dd9624ff4e791f68f6bf4b38b44a81480f0c
Jhon Arias <7honarias@gmail.com>
2020-10-22 01:41:24 +0000

    [ADD] - Exercise 3 cal

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,65 @@
+# include "calc.h"
+# include <stdio.h>
+
+/**
+ * op_add - function for +
+ * @a: firts number
+ * @b: second number
+ * Return: int
+ */
+
+int opt_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function for -
+ * @a: firts number
+ * @b: second number
+ * Return: int
+ */
+
+int opt_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * op_mul - function for *
+ * @a: firts number
+ * @b: second number
+ * Return: int
+ */
+
+int opt_sub(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_div - function for /
+ * @a: firts number
+ * @b: second number
+ * Return: int
+ */
+
+int opt_sub(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * op_sub - function for %
+ * @a: firts number
+ * @b: second number
+ * Return: int
+ */
+
+int opt_sub(int a, int b)
+{
+	return (a % b);
+}
