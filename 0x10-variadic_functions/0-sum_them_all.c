9f4e080d7ab48d40783c776f990c5478013904a4
Jhon Arias <7honarias@gmail.com>
2020-10-23 14:37:12 +0000

    [ADD] - Exercise sum

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum all its parameters
+ * @n: numbers to sum
+ * Return: sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+
+	unsigned int i = 0;
+	unsigned int count;
+
+	va_list ap;
+
+	va_start(ap, n);
+
+	if (n == 0)
+		return (0);
+	for (count = 0; count < n; count++)
+		i += va_arg(ap, int);
+	va_end(ap);
+	return (i);
+}
