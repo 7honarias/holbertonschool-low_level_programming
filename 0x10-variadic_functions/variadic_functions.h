3158aa3b4f511013a364d4d4ba335ebd133d462e
Jhon Arias <7honarias@gmail.com>
2020-10-23 19:46:53 +0000

    UPDATE - header with exercise 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -10,4 +10,16 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+typedef struct op
+{
+	char *str;
+	void (*option)();
+}op_t;
+
+void print_int(va_list arg);
+void print_char(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
+
+
 #endif
