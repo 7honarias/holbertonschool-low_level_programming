6e2ec4519966e02b67b2d10233bc0e5bd8996552
Jhon Arias <7honarias@gmail.com>
2020-10-24 17:52:42 +0000

    Correction if string is null print (nil)

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,17 +11,27 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list str;
+	char *p;
 
 	va_start(str, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(str, char*));
+		p = va_arg(str, char*);
+		if (p)
+		{
+		printf("%s", p);
+		}
+		else
+		{
+			printf("(nil)");
+		}
 		if (i != (n - 1 && separator != NULL))
 		{
 			printf("%s", separator);
 		}
 	}
+	va_end(str);
 	printf("\n");
 
 }
