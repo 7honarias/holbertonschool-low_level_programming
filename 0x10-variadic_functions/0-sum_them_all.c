cad9f114b154c699c9888c8d0aca205d8a532c02
Jhon Arias <7honarias@gmail.com>
2020-10-24 17:17:18 +0000

    Correction var in function

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,25 +1,26 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - sum all its parameters
- * @n: numbers to sum
- * Return: sum
+ * sum_them_all - sum of all parameters
+ * @n: const unsigned int type
+ * Return: all sums.
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
+	unsigned int sum = 0;
+	unsigned int i;
+	va_list addlist;
 
-	unsigned int i = 0;
-	unsigned int count;
-
-	va_list ap;
-
-	va_start(ap, n);
+	va_start(addlist, n);
 
 	if (n == 0)
 		return (0);
-	for (count = 0; count < n; count++)
-		i += va_arg(ap, unsigned int);
-	va_end(ap);
-	return (i);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(addlist, unsigned int);
+	}
+	va_end(addlist);
+
+	return (sum);
 }
