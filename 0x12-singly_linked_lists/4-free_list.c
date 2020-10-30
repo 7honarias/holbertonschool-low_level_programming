abd2b942f09a9b0817e386e6d83ebd22f776150c
Jhon Arias <7honarias@gmail.com>
2020-10-30 15:51:14 +0000

    [ADD] - frees malloc

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t
+ * @head: list give
+ * Return: Nothing void
+ *
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+	if (head != NULL)
+	{
+		while (head->next != NULL)
+		{
+			temp = head;
+			head = head->next;
+			free (temp);
+		}
+	}
+
+}
