4e9b126ebe93ea4461b9339fff38868c2d54a2cc
Jhon Arias <7honarias@gmail.com>
2020-10-30 13:49:54 +0000

    [UPDATE] - correction comment

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,17 +1,18 @@
 #include "lists.h"
-/**
- * add_node - adds a new node at the beginnin
- * @head: of node
- * @str: string
- * Return: list
- */
+
 int _len(const char *s);
 
-list_t* createNodo(const char *str)
+/**
+ * createNodo - function create new nodo
+ * @str: string for include in the nodo
+ * Return: list
+ */
+
+list_t *createNodo(const char *str)
 {
 	list_t *new = NULL;
 
-	new = (list_t*)malloc(sizeof(list_t));
+	new = (list_t *)malloc(sizeof(list_t));
 	if (new != NULL)
 	{
 		new->str = strdup(str);
@@ -20,10 +21,16 @@ list_t* createNodo(const char *str)
 	}
 	return (new);
 }
+/**
+ * add_node - adds a new node at the beginnin
+ * @head: of node
+ * @str: string
+ * Return: list
+ */
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t* new = NULL;
+	list_t *new = NULL;
 
 	new = createNodo(str);
 	if (new != NULL)
@@ -36,8 +43,13 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 	return (new);
-
 }
+
+/**
+ * _len - count of string
+ * @s: string
+ * Return: int number
+ */
 
 int _len(const char *s)
 {
