30cb25ef23301313356750b6b81610b544baf66b
Jhon Arias <7honarias@gmail.com>
2020-11-05 20:54:22 +0000

    [ADD] - first commit for exercise

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - converts a binary number to int
+ * @b: string to convert
+ * Return: int.
+ *
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int c = 0;
+	unsigned int x = 1;
+	int i = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+		c <<= 1;
+		if (b[i] == '1')
+		{
+			c ^= b2;
+		}
+		i++;
+	}
+
+	return (c);
+}
