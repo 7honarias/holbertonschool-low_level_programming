29980fe38470794ad1938194fc297f2e9ed2ee64
Jhon Arias <7honarias@gmail.com>
2020-11-10 03:43:02 +0000

    Correction of exercise 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,11 +1,10 @@
 #include "holberton.h"
+
 /**
- * errorMsg - show error and exit
- * @exitCode: flaq STDERR_FILENO
- * @msg: mensage to show
- * @fileName: name of file to fail
- * Return: Nothing void
- *
+ * errorMsg - Print error message
+ * @exitCode: exit code to stop
+ * @msg: the error message to be printed
+ * @fileName: the name of the file
  */
 
 void errorMsg(int exitCode, const char *msg, const char *fileName)
@@ -13,50 +12,51 @@ void errorMsg(int exitCode, const char *msg, const char *fileName)
 	dprintf(STDERR_FILENO, "%s %s\n", msg, fileName);
 	exit(exitCode);
 }
+
 /**
- * main - function copy file in other file
- * @ac: number of string
- * @av: strings
- * Return: Always 0
+ * main - C program that copied file content from one to another.
+ * @argc: size of array argv and number of command line arguments.
+ * @argv: An  array  of size argc.
+ * Return: Always successful.
  */
-
-
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
-	int file_from, file_to, fdread, fdwrite, cl_from, cl_to;
-	char buffer[1024];
+	int fd_from, fd_to, rd, wr, bz = 1024, cl_from, cl_to;
+	char buf[1024];
 
-	if (ac != 3)
+	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_form file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	file_from = open(av[1], O_RDONLY);
-	if (file_from == -1)
-		errorMsg(98, "Error: Can't read from file", av[1]);
+	fd_from = open(argv[1], O_RDONLY);
 
-	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (file_to == -1)
-		errorMsg(99, "Error: Can't write to NAME_OF_THE_FILE", av[2]);
+	if (fd_from == -1)
+		errorMsg(98, "Error: Can't read from file", argv[1]);
 
-	for (fdread = read(file_from, buffer, 1024); fdread > 0;
-	     fdread = read(file_from, buffer, 1024))
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (fd_to == -1)
+		errorMsg(99, "Error: Can't write to", argv[2]);
+
+	for (rd = read(fd_from, buf, bz); rd > 0; rd = read(fd_from, buf, bz))
 	{
-		fdwrite = write(file_to, buffer, fdread);
-		if (fdwrite == -1)
-			errorMsg(99, "Error: Can't write to", av[2]);
-
+		wr = write(fd_to, buf, rd);
+		if (wr == -1)
+			errorMsg(99, "Error: Can't write to", argv[2]);
 	}
-	if (fdread == -1)
-		errorMsg(98, "Error: Can't close fd", av[1]);
+	if (rd == -1)
+		errorMsg(98, "Error: Can't read from file", argv[1]);
 
-	cl_from = close(file_from);
+	cl_from = close(fd_from);
+
 	if (cl_from == -1)
-		errorMsg(100, "Error: Can't close fd", av[1]);
+		errorMsg(100, "Error: Can't close fd", argv[1]);
 
-	cl_to = close(file_to);
+	cl_to = close(fd_to);
+
 	if (cl_to == -1)
-		errorMsg(100, "Error: Can't close fd", av[2]);
+		errorMsg(100, "Error: Can't close fd", argv[2]);
 
 	return (0);
 }
