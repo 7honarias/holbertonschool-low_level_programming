5adde52a9484163470553970d9c1ad99d5e70708
Jhon Arias <7honarias@gmail.com>
2020-12-14 12:33:13 +0000

    [UPDATE] - Correction h == NULL

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,6 +15,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new = NULL;
 	unsigned int j = 1;
 
+	if (h == NULL && *h)
+		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
