6c0f4ef33f15fb930683c86c89bf84f16d4cfaaa
Jhon Arias <7honarias@gmail.com>
2021-01-15 11:30:59 +0000

    [ADD] - correction 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node = NULL, *temp = NULL;
 	unsigned long int index_key = 0;
 
-	if (ht)
+	if (ht && key && key != "")
 	{
 		index_key = key_index((unsigned char *)key, ht->size);
 		temp = ht->array[index_key];
