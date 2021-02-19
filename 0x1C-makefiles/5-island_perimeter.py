73298d108e89533f87926d71d6218ccea86e3758
7honarias <7honarias@gmail.com>
2021-02-19 21:40:29 +0000

    [UPDATE] - correction island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,14 +7,17 @@ def island_perimeter(grid):
     grid is a list of list of integers"""
 
     perimeter = 0
+
     for m_list in grid:
         perimeter += m_list.count(1) * 4
 
-    
+    for m_list in grid:
+        for i in range(len(m_list)-1):
+            if m_list[i] == 1 and m_list[i+1] == 1:
+                perimeter -=2
+
     for x in range(len(grid)-1):
-        for y in range(len(grid[x])-1):
-            if grid[x][y] == 1 and grid[x][y+1] == 1:
-                perimeter -= 2
+        for y in range(len(grid[0])):
             if grid[x][y] == 1 and grid[x+1][y] == 1:
                 perimeter -= 2
 
